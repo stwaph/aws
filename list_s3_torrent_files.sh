fa7b46ef9dc8d80d82b2e1e7c0d0fe5519f707e7
stwaph <stwaph@gmail.com>
2024-04-29 14:07:41 -0600

    Update list_s3_torrent_files.sh
    fixed --accound-id parameter issue

--- list_s3_torrent_files.sh
+++ list_s3_torrent_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set file extension for search
-FILE_EXTENSION=".torrent"
+FILE_EXTENSION=".txt"
 
 # Get the list of accounts in the organization
 ACCOUNTS=$(aws organizations list-accounts --query 'Accounts[*].Id' --output text)
@@ -11,8 +11,16 @@ for ACCOUNT_ID in $ACCOUNTS
 do
     echo "Account: $ACCOUNT_ID"
 
+    # Switch to the account
+    AWS_ACCESS_KEY_ID=$(aws sts get-caller-identity --query 'Arn' --output text | cut -d'/' -f2)
+    AWS_SECRET_ACCESS_KEY=$(aws sts get-session-token --serial-number "arn:aws:iam::$ACCOUNT_ID:user/$AWS_ACCESS_KEY_ID" --token-code $TOKEN_CODE --query 'Credentials.SecretAccessKey' --output text)
+    AWS_SESSION_TOKEN=$(aws sts get-session-token --serial-number "arn:aws:iam::$ACCOUNT_ID:user/$AWS_ACCESS_KEY_ID" --token-code $TOKEN_CODE --query 'Credentials.SessionToken' --output text)
+    export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+    export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+    export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
+
     # Get the list of buckets in the account
-    BUCKETS=$(aws s3 ls --output text --query 'Buckets[].Name' --account-id $ACCOUNT_ID)
+    BUCKETS=$(aws s3 ls --output text --query 'Buckets[].Name')
 
     # Iterate through each bucket
     for BUCKET in $BUCKETS
@@ -20,6 +28,6 @@ do
         echo "  Bucket: $BUCKET"
 
         # List objects in the bucket with the specified file extension
-        aws s3 ls "s3://$BUCKET" --recursive --human-readable --summarize | grep "$FILE_EXTENSION"
+        aws s3 ls "s3://$BUCKET" --recursive | grep "$FILE_EXTENSION$"
     done
 done
